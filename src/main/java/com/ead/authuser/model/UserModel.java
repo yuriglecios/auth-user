609d89f4954edb24a39b9dff9e46695cc1734d65
yuri glecios <glleciosyuri@gmail.com>
2025-05-10 23:37:02 -0300

    Adicionando mapeamento jpa

--- src/main/java/com/ead/authuser/model/UserModel.java
+++ src/main/java/com/ead/authuser/model/UserModel.java
@@ -2,30 +2,46 @@ package com.ead.authuser.model;
 
 import com.ead.authuser.model.enums.UserStatus;
 import com.ead.authuser.model.enums.UserType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.persistence.*;
+import lombok.Data;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 @Entity
 @Table(name = "TB_USERS")
 public class UserModel implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID userId;
+    @Column(nullable = false, unique = true, length = 50)
     private String username;
+    @Column(nullable = false, unique = true, length = 50)
     private String email;
+    @Column(nullable = false)
     private String password;
+    @Column(nullable = false, length = 250)
     private String fullName;
+    @Column(length = 20)
     private String phoneNumber;
+    @Column(length = 20)
     private String cpf;
+    @Column
     private String imageUrl;
+    @Column(nullable = false)
     private LocalDateTime creationDate;
+    @Column(nullable = false)
     private LocalDateTime lastUpdateDate;
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
     private UserStatus userStatus;
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
     private UserType userType;
 }
