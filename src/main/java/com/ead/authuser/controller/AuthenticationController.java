f925ffffbae57b44161fdfec04d4c12971895943
yuri glecios <glleciosyuri@gmail.com>
2025-05-15 22:22:56 -0300

    Iniciando o controller para o controle e criação de login

---
+++ src/main/java/com/ead/authuser/controller/AuthenticationController.java
@@ -0,0 +1,33 @@
+package com.ead.authuser.controller;
+
+import com.ead.authuser.dto.UserDTO;
+import com.ead.authuser.model.UserModel;
+import com.ead.authuser.service.UserService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RequestMapping("/auth")
+public class AuthenticationController {
+
+    private final UserService userService;
+
+    public AuthenticationController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<Object> signup(@RequestBody UserDTO userDTO) {
+        var userModel = new UserModel();
+        BeanUtils.copyProperties(userModel, userDTO);
+        userModel.setCreationDate(LocalDateTime.now(ZoneId.of("UTC")));
+        userModel.setLastUpdateDate(LocalDateTime.now(ZoneId.of("UTC")));
+        //salvar
+        return null;
+    }
+}
---
+++ src/main/java/com/ead/authuser/dto/UserDTO.java
@@ -0,0 +1,16 @@
+package com.ead.authuser.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class UserDTO {
+    private String username;
+    private String email;
+    private String password;
+    private String fullName;
+    private String phoneNumber;
+    private String cpf;
+    private String imageUrl;
+}
